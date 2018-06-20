8fb5988a2e17a80d7c586684623a9e1f114dd64a
Joe Badaczewski <badaczewskij@live.com>
2018-06-20 13:37:23 -0400

    8) Resetting the Quiz

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 
 class QuizEnd extends Component {
+
+    handleResetQuiz(e) {
+        e.preventDefault();
+        this.props.resetClickHandler()
+    }
+
     render() {
         return (
             <div>
                 <p>Thanks for playing!</p>
-                <a href=''>Reset Quiz</a>
+                <a onClick={this.handleResetQuiz.bind(this)} href=''>Reset Quiz</a>
             </div>
         );
     }
