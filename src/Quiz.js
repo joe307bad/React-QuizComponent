66d940d5b348e11cdda9752daad93c6879f48441
Joe Badaczewski <badaczewskij@live.com>
2018-06-20 12:35:29 -0400

    no message

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 let quizData = require('./quiz_data.json')
 
-export default class Quiz extends Component {
+class Quiz extends Component {
 
     constructor(props) {
         super(props);
@@ -20,3 +20,5 @@ export default class Quiz extends Component {
         );
     }
 }
+
+export default Quiz;
